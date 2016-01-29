9971f6f3550daf9c12fdf1631bbed9504b685c40
kaklise <kaklise@.sandia.gov>
2016-01-29 09:48:01 -0700

    updated simple example using config

--- examples/simple/simple_driver_with_config.py
+++ examples/simple/simple_driver_with_config.py
@@ -1,8 +1,9 @@
 import pecos
-import yaml
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import numpy as np
+import yaml
 
 # Input
 system_name = 'Simple'
@@ -21,26 +22,29 @@ time_filter = config['Time Filter']
 corrupt_values = config['Corrupt Values']
 range_bounds = config['Range Bounds']
 increment_bounds = config['Increment Bounds']
-    
+   
+ # Define output files and directories
+results_directory = 'Results'
+if not os.path.exists(results_directory):
+    os.makedirs(results_directory)
+results_subdirectory = os.path.join(results_directory, system_name + '_2015_01_01')
+if not os.path.exists(results_subdirectory):
+    os.makedirs(results_subdirectory)
+metrics_file = os.path.join(results_directory, system_name + '_metrics.csv')
+test_results_file = os.path.join(results_subdirectory, system_name + '_test_results.csv')
+report_file =  os.path.join(results_subdirectory, system_name + '.html')
+
 # Create an PerformanceMonitoring instance
-pm = pecos.PerformanceMonitoring()
+pm = pecos.monitoring.PerformanceMonitoring()
 
 # Populate the PerformanceMonitoring instance
 df = pd.read_excel(data_file)
 pm.add_dataframe(df, system_name)
 pm.add_translation_dictonary(trans, system_name)
 
-# Set options to create results directories
-pm.options.results_directory = 'Results'
-pm.options.results_subdirectory_root = system_name
-pm.options.results_subdirectory_prefix = df.index.date[0].strftime('_%Y_%m_%d') + '_withconfig'
-pm.options.make_results_directory()
-pm.options.make_results_subdirectory()
-pm.options.clean_results_subdirectory()
-
 # Check timestamp
-pm.check_timestamp(specs['Frequency']) 
-        
+pm.check_timestamp(specs['Frequency'])
+ 
 # Generate time filter
 time_filter = pm.evaluate_string('Time Filter', time_filter)
 pm.add_time_filter(time_filter)
@@ -52,10 +56,13 @@ pm.check_missing()
 pm.check_corrupt(corrupt_values) 
 
 # Add composite signals
-for composite_signal in composite_signals:
-    for key,value in composite_signal.items():
-        signal = pm.evaluate_string(key, value, specs)
-        pm.add_signal(key, signal)
+elapsed_time= pm.get_elapsed_time()
+wave_model = np.sin(10*(elapsed_time/86400))
+wave_model.columns=['Wave Model']
+pm.add_signal('Wave Model', wave_model)
+wave_mode_abs_error = np.abs(np.subtract(pm.df[pm.trans['Wave']], wave_model))
+wave_mode_abs_error.columns=['Wave Absolute Error C', 'Wave Absolute Error D']
+pm.add_signal('Wave Absolute Error', wave_mode_abs_error)
 
 # Check range
 for key,value in range_bounds.items():
@@ -65,15 +72,17 @@ for key,value in range_bounds.items():
 for key,value in increment_bounds.items():
     pm.check_increment(value, key) 
     
-# Generate QC metrics
-QCI = pm.compute_QCI()
-    
+# Compute metrics
+QCI = pecos.metrics.qci(pm)
+ 
 # Create a custom graphic
-df.plot(ylim=[-1.5,1.5])
-plt.savefig(os.path.join(pm.options.results_subdirectory, system_name+'_custom_1.jpg')) 
+plt.figure(figsize = (7.0,3.5))
+ax = plt.gca()
+df.plot(ax=ax, ylim=[-1.5,1.5])
+plt.savefig(os.path.join(results_subdirectory, system_name+'_custom_1.jpg')) 
 
-# Generate report
-pm.group_test_results()
-pm.write_performance_metric_file(QCI)
-pm.write_test_results_file()
-pm.write_HTML_report(config)
+# Write metrics, test results, and report files
+pecos.io.write_metrics(metrics_file, QCI)
+pecos.io.write_test_results(test_results_file, pm.test_results)
+pecos.io.write_monitoring_report(report_file, results_subdirectory, pm, QCI)
+
