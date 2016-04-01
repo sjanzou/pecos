ed554df35d5e72bb566d0b30031e542f1ba8ea65
kaklise <kaklise@.sandia.gov>
2016-04-01 13:48:16 -0600

    updated setup.py and min requirements file

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup, find_packages
 from distutils.core import Extension
 
-setup(name='pecos',
-    version='0.1',
-    description='Performance Monitoring',
-    url='',
-    license='Revised BSD',
-    packages=find_packages(),
-    zip_safe=False)
+DISTNAME = 'pecos'
+VERSION = '0.1'
+PACKAGES = ['pecos']
+EXTENSIONS = []
+DESCRIPTION = 'Python package for performance monitoring of time series data.'
+LONG_DESCRIPTION = open('README.md').read()
+AUTHOR = 'Katherine Klise, Joshua Stein'
+MAINTAINER_EMAIL = 'kaklise@sandia.gov'
+LICENSE = 'Revised BSD'
+URL = 'https://github.com/kaklise/pecos'
 
+setup(name=DISTNAME,
+      version=VERSION,
+      packages=PACKAGES,
+      ext_modules=EXTENSIONS,
+      description=DESCRIPTION,
+      long_description=LONG_DESCRIPTION,
+      author=AUTHOR,
+      maintainer_email=MAINTAINER_EMAIL,
+      license=LICENSE,
+      url=URL,
+      zip_safe=False)
