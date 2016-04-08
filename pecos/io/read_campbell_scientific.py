0d62d4e1f62ec23b7ef114446bab8c02c1ec4c26
Will Holmgren <william.holmgren@gmail.com>
2016-04-08 12:14:55 -0700

    Dataframe to DataFrame

--- pecos/io/read_campbell_scientific.py
+++ pecos/io/read_campbell_scientific.py
@@ -6,20 +6,20 @@ logger = logging.getLogger(__name__)
 def read_campbell_scientific(file_name, index_col='TIMESTAMP', encoding=None):
     """
     Read Campbell Scientific CSV file
-    
+
     Parameters
     ----------
     file_name : string
         File name with full path
-        
+
     index_col : string
         Index column name
-    
+
     encoding : string
         character encoding (i.e. utf-16)
     """
     logger.info("Reading Campbell Scientific CSV file " + file_name)
-    
+
     try:
         df = pd.read_csv(file_name, skiprows=1, encoding=encoding, index_col=index_col, parse_dates=True, dtype=unicode) #, low_memory=False)
         df = df[2:]
@@ -29,13 +29,13 @@ def read_campbell_scientific(file_name, index_col='TIMESTAMP', encoding=None):
         df = pd.DataFrame(data = df.values, index = index, columns = df.columns, dtype='float64')
     except:
         logger.warning("Cannot extract database, CSV file reader failed " + file_name)
-        df = pd.Dataframe()
+        df = pd.DataFrame()
         return
-    
+
     # Drop rows with NaT (not a time) in the index
     try:
         df.drop(pd.NaT, inplace=True)
     except:
         pass
-    
+
     return df
