ec91ea0ca7875a11561aed40619299b2ec3dd854
kaklise <kaklise@.sandia.gov>
2016-04-14 09:45:40 -0600

    added isfile to test

--- pecos/tests/test_read_campbell_scientific_file.py
+++ pecos/tests/test_read_campbell_scientific_file.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from os.path import abspath, dirname, join
+from os.path import abspath, dirname, join, isfile
 import pecos
 import inspect
 
@@ -8,6 +8,8 @@ datadir = abspath(join(testdir, 'data'))
 
 def test_read_campbell_scientific():
     file_name = join(datadir,'TEST_db1_2014_01_01.dat')
+    assert_true(isfile(file_name))
+    
     df = pecos.io.read_campbell_scientific(file_name, 'TIMESTAMP')
     assert_equals((48,11), df.shape)
     
