2715cef40e99d050d7a8091aa3e8b56c851391ce
kaklise <kaklise@.sandia.gov>
2016-04-14 09:18:26 -0600

    changed dirname(abspath(__file__)) to dirname(abspath(inspect.getfile(inspect.currentframe())))

--- pecos/tests/test_read_campbell_scientific_file.py
+++ pecos/tests/test_read_campbell_scientific_file.py
@@ -2,9 +2,9 @@ from nose.tools import *
 from os.path import abspath, dirname, join
 import pecos
 
-testdir = dirname(abspath(__file__))
-datadir = join(testdir,'data')
- 
+testdir = dirname(abspath(inspect.getfile(inspect.currentframe())))
+datadir = abspath(join(testdir, 'data'))    
+
 def test_read_campbell_scientific():
     file_name = join(datadir,'TEST_db1_2014_01_01.dat')
     df = pecos.io.read_campbell_scientific(file_name, 'TIMESTAMP')
