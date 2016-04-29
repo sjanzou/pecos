4e3a7de888be45d780876f390001c02c064e97db
kaklise <kaklise@.sandia.gov>
2016-04-29 08:31:46 -0600

    update to get examples tested

--- pecos/tests/test_examples.py
+++ pecos/tests/test_examples.py
@@ -12,7 +12,7 @@ def test_simple_example():
     exampledir = exampledir = join(testdir,'..', '..', 'examples', 'simple')
     
     os.chdir(exampledir)
-    retcode = subprocess.call([sys.executable, 'simple_example.py'],  shell=True)
+    retcode = subprocess.call(['simple_example.py'],  shell=True)
     os.chdir(cwd)
     
     assert_equals(retcode, 0)
@@ -22,7 +22,7 @@ def test_pv_example():
     exampledir = exampledir = join(testdir,'..', '..', 'examples', 'pv')
     
     os.chdir(exampledir)
-    retcode = subprocess.call([sys.executable, 'pv_example.py'],  shell=True)
+    retcode = subprocess.call(['pv_example.py'],  shell=True)
     os.chdir(cwd)
     
     assert_equals(retcode, 0)
@@ -32,7 +32,7 @@ def test_metrics_example():
     exampledir = exampledir = join(testdir,'..', '..', 'examples', 'metrics')
     
     os.chdir(exampledir)
-    retcode = subprocess.call([sys.executable, 'metrics_example.py'],  shell=True)
+    retcode = subprocess.call(['metrics_example.py'],  shell=True)
     os.chdir(cwd)
     
     assert_equals(retcode, 0)
@@ -42,10 +42,7 @@ def test_dashboard_example():
     exampledir = exampledir = join(testdir,'..', '..', 'examples', 'dashboard')
     
     os.chdir(exampledir)
-    retcode = subprocess.call([sys.executable, 'dashboard_example.py'],  shell=True)
+    retcode = subprocess.call(['dashboard_example.py'],  shell=True)
     os.chdir(cwd)
     
     assert_equals(retcode, 0)
-
-if __name__ == '__main__':
-    p = test_simple_example()
