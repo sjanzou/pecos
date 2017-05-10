834ad27d6623aaa3393f1e24d4e4e48cb7682b8f
kaklise <kaklise@sandia.gov>
2017-05-09 18:03:12 -0600

    Removed pvlib test

--- pecos/tests/test_pv.py
+++ pecos/tests/test_pv.py
@@ -10,44 +10,44 @@ import pecos
 testdir = dirname(abspath(__file__))
 datadir = join(testdir,'data')
 
-def test_sun_position_pvlib():
-
-    # Albuquerque
-    # Sunrise on 12/10/2015 = 7:04, 118 degrees
-    # Subset on 12/10/2015 = 4:55, 242 degrees
-    
-    timeseries = pd.DatetimeIndex(pd.date_range('12/10/2015', periods=24*60, freq='Min'))
-    timeseries = timeseries.tz_localize('MST')
-    
-    # pvlib python
-    Location = pvlib.location.Location
-    Location.latitude = 35.04;
-    Location.longitude = -106.62;
-    #Location.altitude = 1619;
-    #Location.tz = 'US/Mountain'
-    
-    sun_position = pvlib.solarposition.ephemeris(timeseries, Location.latitude, Location.longitude);
-    sun_position.index = sun_position.index.tz_localize(tz=None)
-    
-    #sun_position.plot()
-    
-    sunrise_el = sun_position.loc['2015-12-10 7:04:00', 'apparent_elevation']
-    expected = 0
-    assert_less(abs(sunrise_el-expected), 0.5) # 0.5 degree
-    
-    sunset_el = sun_position.loc['2015-12-10 16:55:00', 'apparent_elevation']
-    expected = 0
-    assert_less(abs(sunset_el-expected), 0.5) # 0.5 degree
-    
-    sunrise_az = sun_position.loc['2015-12-10 7:04:00', 'azimuth']
-    expected = 118
-    assert_less(abs(sunrise_az-expected), 0.5) # 0.5 degree
-    
-    sunset_az = sun_position.loc['2015-12-10 16:55:00', 'azimuth']
-    expected = 242
-    assert_less(abs(sunset_az-expected), 0.5) # 0.5 degree
-
-    return sun_position
+#def test_sun_position_pvlib():
+#
+#    # Albuquerque
+#    # Sunrise on 12/10/2015 = 7:04, 118 degrees
+#    # Subset on 12/10/2015 = 4:55, 242 degrees
+#    
+#    timeseries = pd.DatetimeIndex(pd.date_range('12/10/2015', periods=24*60, freq='Min'))
+#    timeseries = timeseries.tz_localize('MST')
+#    
+#    # pvlib python
+#    Location = pvlib.location.Location
+#    Location.latitude = 35.04;
+#    Location.longitude = -106.62;
+#    #Location.altitude = 1619;
+#    #Location.tz = 'US/Mountain'
+#    
+#    sun_position = pvlib.solarposition.ephemeris(timeseries, Location.latitude, Location.longitude);
+#    sun_position.index = sun_position.index.tz_localize(tz=None)
+#    
+#    #sun_position.plot()
+#    
+#    sunrise_el = sun_position.loc['2015-12-10 7:04:00', 'apparent_elevation']
+#    expected = 0
+#    assert_less(abs(sunrise_el-expected), 0.5) # 0.5 degree
+#    
+#    sunset_el = sun_position.loc['2015-12-10 16:55:00', 'apparent_elevation']
+#    expected = 0
+#    assert_less(abs(sunset_el-expected), 0.5) # 0.5 degree
+#    
+#    sunrise_az = sun_position.loc['2015-12-10 7:04:00', 'azimuth']
+#    expected = 118
+#    assert_less(abs(sunrise_az-expected), 0.5) # 0.5 degree
+#    
+#    sunset_az = sun_position.loc['2015-12-10 16:55:00', 'azimuth']
+#    expected = 242
+#    assert_less(abs(sunset_az-expected), 0.5) # 0.5 degree
+#
+#    return sun_position
 
 def test_sun_position_ephem():
     
